f68a6926e34c6234795e3fe444ac05ebec4bb543
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-08-25 21:07:40 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file
+# 0-create_a_file.pp
+
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
+  ensure  => present,
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
