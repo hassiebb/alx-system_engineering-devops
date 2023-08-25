4be13ee22733ca21e0e192f0128eae3df116ad70
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-08-25 21:43:18 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,7 +3,6 @@
 package { 'python3-pip':
   ensure => installed,
 }
-
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
